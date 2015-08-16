cc67b75436ba7bbb15f467887834eaf9053513c3
Max Schram <maxbschram@gmail.com>
2015-08-16 23:56:57 +0000

    Precompile assets

---
+++ public/assets/sites/index_item-41fa5bf621c2a172b1651ebde676e25a0711ebc5f99dac2c5585afcb41cbacdf.js
@@ -0,0 +1,2 @@
+(function() { this.JST || (this.JST = {}); this.JST["sites/index_item"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<li class="site-index-item">\n  <a>',  site.escape("title") ,'</a>\n</li>\n');}return __p.join('');};
+}).call(this);
