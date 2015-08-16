88af7826eacd32c61fe025ba69825b8ecfac989a
Max Schram <maxbschram@gmail.com>
2015-08-16 02:46:18 +0000

    Add link to sites index

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   def self.find_by_credentials(email, password)
     user = User.find_by({ email: email })
-    user && user.has_password?(password) ? user : password
+    user && user.has_password?(password) ? user : nil
   end
 
   def has_password?(password)
