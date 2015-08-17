2c704e4f0d87be217d11ab7f233349c7f73e5927
Max Schram <maxbschram@gmail.com>
2015-08-17 12:04:38 -0700

    Add signup to landing page

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
   $(".guest-login").on("click", function (e) {
     e.preventDefault();
+    $(e.currentTarget).blur();
     $("#email").val("guest");
     $("#password").val("password");
     $(".login-form").submit();
+  });
+
+  $(".new-account").on("click", function (e) {
+    e.preventDefault();
+    $(e.currentTarget).blur();
+    $(".submit").html("Sign Up");
+    $("form").attr("action", "signup");
+  });
+
+  $(".login").on("click", function (e) {
+    e.preventDefault();
+    $(e.currentTarget).blur();
+    $(".submit").html("Login");
+    $("form").attr("action", "login");
   });
 });
