485c7b8d607cb6ea6aee74d823f56345417f7c37
Max Schram <maxbschram@gmail.com>
2015-08-18 15:07:19 -0700

    Build page html for downloading

--- app/assets/javascripts/models/element.js
+++ app/assets/javascripts/models/element.js
@@ -6,5 +6,24 @@ Webspinr.Models.Element = Backbone.Model.extend({
     var json = Backbone.Model.prototype.toJSON.call(this);
     json.attrs = JSON.stringify(this.get("attrs"));
     return json;
-  }
+  },
+
+  toHTML: function () {
+    var html = $("<" + this.get("tag") + ">");
+    var attrs = this.get("attrs");
+      for (var attr in attrs) {
+        if (attr === "style") {
+          for(var prop in attrs[attr]) {
+            html.css(prop, attrs[attr][prop]);
+          }
+        } else if (attr === 'class') {
+          attrs[attr].forEach(function (klass) {
+            html.addClass(klass);
+          });
+        } else {
+          html.attr(attr, attrs[attr]);
+        }
+      }
+      return html.html(this.escape("content"));
+    }
 });
--- app/assets/javascripts/views/editor/sites/index_item.js
+++ app/assets/javascripts/views/editor/sites/index_item.js
@@ -4,11 +4,29 @@ Webspinr.Views.SitesIndexItem = Backbone.View.extend({
 
   events: {
     "click .edit": "editSite",
-    "click .live": "viewSite"
+    "click .live": "viewSite",
+    "click .download": "downloadSite"
   },
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
+  },
+
+  downloadSite: function (e) {
+    this.model.fetch({
+      success: function () {
+        var page;
+        var pageHTML;
+        this.model.pages().each(function (page) {
+          pageHTML = "";
+          page.elements().each(function (element) {
+            pageHTML += element.toHTML()[0].outerHTML + "\n";
+          });
+          // console.log(pageHTML);
+        }.bind(this));
+      }.bind(this)
+    });
+    $(e.currentTarget).blur();
   },
 
   viewSite : function () {
