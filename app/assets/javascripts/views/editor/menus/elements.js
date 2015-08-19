6fde706bb1235b7e9da79bfc300d3891c56550a1
Max Schram <maxbschram@gmail.com>
2015-08-19 12:02:30 -0700

    Change text to span tags

--- app/assets/javascripts/views/editor/menus/elements.js
+++ app/assets/javascripts/views/editor/menus/elements.js
@@ -23,7 +23,7 @@ Webspinr.Views.ElementsMenu = Backbone.View.extend({
 
   createText: function () {
     var element = new Webspinr.Models.Element({
-      tag: "p",
+      tag: "span",
       content: "Click here to enter your text...",
       page_id: this.currentPageView.model.id,
       attrs: {
