e2ca07cc94d68c94116c9607d76c564e89c47cdd
Max Schram <maxbschram@gmail.com>
2015-08-20 17:16:02 -0700

    Add easter egg

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -8,9 +8,22 @@
 //= require jszip.min
 //= require jquery.zoomer
 //= require jquery.overlaps
+//= require cheet.min
 //= require webspinr_editor
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views/editor
 //= require_tree ./routers
+
+var params = [
+  'height='+"930px",
+  'width='+"1653px",
+  "fullscreen=yes"
+].join(',');
+cheet('g i v e m e c a g e', function () {
+var popup = window.open(
+    "https://d12vb6dvkz909q.cloudfront.net/uploads/galleries/35694/wild-at-heart-nicolas-cage-wallpapers.jpg",
+    'popup_window',
+    params);
+});
