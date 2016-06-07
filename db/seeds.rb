c75db73df2f996db2086e151575cd6cf3bf8419f
Max Schram <maxbschram@gmail.com>
2016-06-06 18:04:00 -0700

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,15 @@ guest = User.create(
   {email: "guest", password: "password"}
 )
 guest = User.find_by(email: "guest")
-guest.sites.first.destroy! if guest.sites.count != 0
-site = Site.create!(
+site = guest.sites.first
+site = Site.create(
   {title: "Bay Area Hikes", description: "Great hikes in the Bay", user: guest}
+) unless site
+page = site.pages.first
+page.update!(
+  {title: "index", background_image: "", background_color: "#004d4d", height: 800, width: 1400}
 )
-site.pages.first.destroy!
-page = Page.create!(
-  {title: "index", site: site, background_image: "", background_color: "#004d4d", height: 800, width: 1400}
-)
+page.elements.destroy_all
 Element.create!([
   {content: "", tag: "img", attrs: "{\"style\":{\"top\":\"53.5px\",\"left\":\"0.5px\",\"position\":\"absolute\",\"z-index\":2},\"class\":[\"image\"],\"src\":\"https://www.filepicker.io/api/file/q7JLiwbjTGG2C3glG27p\"}", page: page},
   {content: "(Above) Alamere Falls - Point Reyes National Seashore\r\n\r\nA rewarding walk along the Bolinas Coast Trail turns into a hike past mountain lakes to this enchanted waterfall. \r\n\r\nDuring some months, you may catch a whale migration out at sea.\r\n\r\n(Right) Skyline to the Sea Trail - Big Basin\r\n\r\nA manageable 2.5 hour, moderate hike from park headquarters takes you to an incredible ocean view summit. \r\n\r\nAt the top a sandy path reminds you that this was once the bottom of the ocean.", tag: "p", attrs: "{\"style\":{\"top\":\"523.5px\",\"left\":\"74.5px\",\"position\":\"absolute\",\"font-size\":\"16px\",\"z-index\":3},\"class\":[\"text\"]}", page: page},
